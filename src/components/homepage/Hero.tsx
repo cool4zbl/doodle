e82585a6c8dfee96b0e361566fc136b6051e5649
Binliu Zhang <binliu.zhang@gmail.com>
2025-08-10 21:36:42 +0200

    feat: update Docusaurus dependencies and enhance hero section styling

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -11,7 +11,10 @@ export const Hero: FunctionComponent = () => {
                 <h1 className={clsx("hero__title", styles.title)}>
                     Hi, I'm{" "}
                     <span className={styles.highlighted}>Binliu Zhang</span>,
-                    <br />a software engineer.
+                    <br />
+                    <span className={styles.job}>
+                    a software engineer.
+                    </span>
                 </h1>
                 <p className={clsx("hero__subtitle", styles.subtitle)}>
                     Open-source enthusiast, interested in personal growth and
